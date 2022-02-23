3af1e0cbee11d5ef534411ba9ceacc34b9de7d52
markgamal32 <96923190+markgamal32@users.noreply.github.com>
2022-02-23 20:41:15 +0200

    Update app.js

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,3 +1,4 @@
+
 /**
  * 
  * Manipulating the DOM exercise.
@@ -14,21 +15,20 @@
 */
 
 /**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
  * Define Global Variables
  * 
 */
-const sections = Array.from(document.querySelectorAll('section'));
-const menue = document.getElementById('navbar_list');
-/*
+// navigation global var
+const navigation = document.getElementById('navbar__list');
+// sections global var
+const sections = document.querySelectorAll('section');
+
+/**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+
 
 
 /**
@@ -38,15 +38,78 @@ const menue = document.getElementById('navbar_list');
 */
 
 // build the nav
-const buildNav = () => 
+
+const navBuilder = () => {
+
+    let navUI = '';
+    // looping over all sections
+    sections.forEach(section => {
+
+        const sectionID = section.id;
+        const sectionDataNav = section.dataset.nav;
+
+        navUI += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;
+
+    });
+    // append all elements to the navigation
+    navigation.innerHTML = navUI;
+
+
+};
+
+navBuilder();
 
 // Add class 'active' to section when near top of viewport
 
+// getting the largest value that's less or equal to the number
+const offset = (section) => {
+    return Math.floor(section.getBoundingClientRect().top);
+};
+
+// remove the active class
+const removeActive = (section) => {
+    section.classList.remove('your-active-class');
+    section.style.cssText = "background-color: linear-gradient(0deg, rgba(255,255,255,.1) 0%, rgba(255,255,255,.2) 100%)";
+};
+// adding the active class
+const addActive = (conditional, section) => {
+    if(conditional){
+        section.classList.add('your-active-class');
+        section.style.cssText = "background-color: yellow;";
+    };
+};
+
+//implementating the actual function
+
+const sectionActivation = () => {
+    sections.forEach(section => {
+        const elementOffset = offset(section);
+
+        inviewport = () => elementOffset < 150 && elementOffset >= -150;
+
+        removeActive(section);
+        addActive(inviewport(),section);
+    });
+};
+
+window.addEventListener('scroll' ,sectionActivation);
 
 // Scroll to anchor ID using scrollTO event
 
+const scrolling = () => {
 
+    const links = document.querySelectorAll('.navbar__menu a');
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            for(i = 0 ; i<sections ; i++){
+                sections[i].addEventListener("click",sectionScroll(link));
+            }
+        });
+    });
 
+};
+
+scrolling();
 
 /**
  * End Main Functions
@@ -59,5 +122,3 @@ const buildNav = () =>
 // Scroll to section on link click
 
 // Set sections as active
-
-
